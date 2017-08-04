8bc1dd1cad8c4b97acc6ac6a83d340ce54e35f7c
LCJ-MinYa <1049468118@qq.com>
2017-08-04 17:16:10 +0800

    测试RN与原生通信

---
+++ index.ios.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import React, {
+	Component
+} from 'react';
+
+import {
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View,
+} from 'react-native';
+
+class NativePort extends Component {
+	render() {
+		return (
+			<View>
+				<Text>
+					测试rn与原生交互
+				</Text>
+			</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+
+});
+
+AppRegistry.registerComponent('NativePort', () => NativePort);
---
+++ ios/CodeInit/Class/WeChat/Controller/LiLiWeChatViewController.m
@@ -0,0 +1,38 @@
+//
+//  LiLiWeChatViewController.m
+//  CodeInit
+//
+//  Created by minya on 2017/4/7.
+//  Copyright © 2017年 minya. All rights reserved.
+//
+
+#import "LiLiWeChatViewController.h"
+#import "UIImage+LiLiImage.h"
+#import <React/RCTRootView.h>
+
+@interface LiLiWeChatViewController ()
+
+@end
+
+@implementation LiLiWeChatViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIBarButtonItem * item = [[UIBarButtonItem alloc] initWithImage:[UIImage imageWithOriginalImageName:@"add"] style:0 target:self action:@selector(activity)];
+    self.navigationItem.rightBarButtonItem = item;
+}
+
+- (void)activity{
+    NSLog(@"点击+号");
+    NSURL *jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios"];
+    RCTRootView *rootView =
+    [[RCTRootView alloc] initWithBundleURL : jsCodeLocation
+                         moduleName        : @"NativePort"
+                         initialProperties : nil
+                          launchOptions    : nil];
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view = rootView;
+    [self presentViewController:vc animated:YES completion:nil];
+}
+
+@end
