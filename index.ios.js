5888e498ef4183b27eea228c3b999b442208eeef
LCJ-MinYa <1049468118@qq.com>
2017-08-08 10:12:14 +0800

    react native通知原生

--- index.ios.js
+++ index.ios.js
@@ -9,7 +9,10 @@ import {
 	StyleSheet,
 	Text,
 	View,
+	NativeModules,
+	TouchableOpacity
 } from 'react-native';
+let CalendarManager = NativeModules.CalendarManager;
 
 class NativePort extends Component {
 	render() {
@@ -18,11 +21,22 @@ class NativePort extends Component {
 				<Text>
 					测试rn与原生交互
 				</Text>
+				<TouchableOpacity
+					onPress={this.doNative.bind(this, '调用testNormalEvent方法', '测试普通调用')}
+				>
+					<Text>1111</Text>
+				</TouchableOpacity>
 			</View>
 		)
 	}
 	componentDidMount() {
 		console.log(this.props);
+		//  调用原生方法
+		console.log(CalendarManager);
+		CalendarManager.testNormalEvent('调用testNormalEvent方法', '测试普通调用');
+	}
+	doNative(one, two) {
+		CalendarManager.testNormalEvent(one, two);
 	}
 }
 
--- ios/CodeInit/Class/WeChat/Controller/LiLiWeChatViewController.m
+++ ios/CodeInit/Class/WeChat/Controller/LiLiWeChatViewController.m
@@ -9,6 +9,27 @@
 #import "LiLiWeChatViewController.h"
 #import "UIImage+LiLiImage.h"
 #import <React/RCTRootView.h>
+#import <React/RCTBridgeModule.h>
+
+@interface CalendarManager : NSObject<RCTBridgeModule>
+
+@end
+
+//为了实现RCTBridgeModule协议，你的类需要包含RCT_EXPORT_MODULE()宏。这个宏也可以添加一个参数用来指定在Javascript中访问这个模块的名字。如果你不指定，默认就会使用这个Objective-C类的名字。
+
+// CalendarManager.m
+@implementation CalendarManager
+
+//  必须实现
+RCT_EXPORT_MODULE();
+
+//  对外提供调用方法（testNormalEvent为方法名，后面为参数，按顺序和对应数据类型在js进行传递）
+RCT_EXPORT_METHOD(testNormalEvent:(NSString *)name forSomething:(NSString *)thing){
+    NSString *info = [NSString stringWithFormat:@"Test: %@\nFor: %@", name, thing];
+    NSLog(@"%@", info);
+}
+
+@end
 
 @interface LiLiWeChatViewController ()
 
